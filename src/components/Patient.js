0db1c4448ba00f25fd635ef07e9c054668b5ace3
Faith Kamau <74059692+nerdhiqedme@users.noreply.github.com>
2022-12-22 06:54:13 +0300

    fixed toggle bugs

--- src/components/Patient.js
+++ src/components/Patient.js
@@ -1,14 +1,12 @@
-import { FaTimes } from 'react-icons/fa'
+
 const Patient = ({patient, onDelete, onToggle}) => {
   return (
     <div>
         className={`patient ${patient.reminder && 'reminder'}`}
       onDoubleClick={() => onToggle(patient.id)}
       <h3>{patient.name}{' '}
-      <FaTimes
-          style={{ color: 'red', cursor: 'pointer' }}
-          onClick={() => onDelete(patient.id)}
-        />
+    //  
+    
       </h3>
       <p> Age:{patient.age}  Gender:{patient.gender}</p>
     </div>
